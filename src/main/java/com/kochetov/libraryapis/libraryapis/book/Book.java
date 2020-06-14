950b40335ab813be19eda344cf0044576cf2cd3e
konstantin kochetov <kochetov0824@gmail.com>
2020-06-14 07:41:07 +0200

    comment

--- src/main/java/com/kochetov/libraryapis/libraryapis/book/Book.java
+++ src/main/java/com/kochetov/libraryapis/libraryapis/book/Book.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
+// this annotation exclude all null values when returning response
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Book {
 
